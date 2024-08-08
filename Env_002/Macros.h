729dbd79007e993277bbe89518757af25eda13e3
Pedram Hashemi <PedramHashemi75@gmail.com>
2024-08-08 14:23:20 +0100

    debug

--- Env_002/Macros.h
+++ Env_002/Macros.h
@@ -2,7 +2,7 @@
 // Macros for ENV_002 : Putter
 #define __CUST_ENV__ Env_Putter
 
-#ifndef _MSC_VER
+#ifdef _MSC_VER
 	#define ROOM_COUNT 10
 	#define MAX_SS_PER_ROOM 3
 	#define FURNITURE_COUNT 30
@@ -17,17 +17,3 @@
 	#define CIRCULATION_GRID_SIZE 4
 	#define WEIGHTS {1.0,5,40,2.0,-1}
 #endif
-
-#define ROOM_COUNT 3
-#define MAX_SS_PER_ROOM 4
-#define FURNITURE_COUNT 4
-#define MAX_X 32
-#define MAX_Y 32
-#define GOAL_AREAS { 0.5,0.2,0.3 }
-#define PENALIZED_CONSTANT 0
-#define ALPHA 0.9
-#define BETA 0.1
-#define MAX_STEPS 20
-#define RESET_GRIDS_COUNT 1
-#define CIRCULATION_GRID_SIZE 4
-#define WEIGHTS {5.0, -1}
